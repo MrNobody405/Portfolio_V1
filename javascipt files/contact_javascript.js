06c173e597331d8d4666502a63a0c1cb1c5988d5
MrNobody405 <82315331+MrNobody405@users.noreply.github.com>
2022-11-05 20:28:32 +0800

    Add files via upload

---
+++ javascipt files/contact_javascript.js
@@ -0,0 +1,15 @@
+﻿function contact(input) {
+		var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+		
+		if (input.value.match(validRegex)) {
+			alert("Thank you for leaving a message, email will be used to contact you soon!")
+			document.form1.text1.focus();
+			return true;
+		} else {
+			alert("Invalid email address!");
+			document.form1.text1.focus();
+			return false;
+		}
+		
+		if(!alert("Invalid email address!") || !alert("Thank you for leaving a message, email will be used to contact you soon!")){window.location.reload();}
+	}
